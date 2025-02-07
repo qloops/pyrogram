7cf65d774c763318726d72af6fea98b6c9a9e800
KurimuzonAkuma <arturgayauskas@gmail.com>
2025-02-07 09:01:10 +0300

    Fix run description

--- pyrogram/methods/utilities/run.py
+++ pyrogram/methods/utilities/run.py
@@ -59,20 +59,6 @@ class Run:
                 app = Client("my_account")
                 ...  # Set handlers up
                 app.run()
-
-            .. code-block:: python
-
-                from pyrogram import Client
-
-                app = Client("my_account")
-
-
-                async def main():
-                    async with app:
-                        print(await app.get_me())
-
-
-                app.run(main())
         """
         loop = asyncio.get_event_loop()
         run = loop.run_until_complete
