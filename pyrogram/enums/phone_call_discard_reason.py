f695f8d61476ff45df522f198c676ab1408ecbf6
KurimuzonAkuma <arturgayauskas@gmail.com>
2025-05-01 11:18:08 +0300

    Update call discard reasons

--- pyrogram/enums/phone_call_discard_reason.py
+++ pyrogram/enums/phone_call_discard_reason.py
@@ -17,20 +17,24 @@
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 from pyrogram import raw
+
 from .auto_name import AutoName
 
 
 class PhoneCallDiscardReason(AutoName):
     """Phone call discard reason enumeration used in :obj:`~pyrogram.types.PhoneCallEnded`."""
 
-    BUSY = raw.types.PhoneCallDiscardReasonBusy
-    "Busy"
-
-    DISCONNECT = raw.types.PhoneCallDiscardReasonDisconnect
-    "Disconnect"
-
-    HANGUP = raw.types.PhoneCallDiscardReasonHangup
-    "Hangup"
-
     MISSED = raw.types.PhoneCallDiscardReasonMissed
-    "Missed"
+    "The call was ended before the conversation started. It was canceled by the caller or missed by the other party"
+
+    DECLINED = raw.types.PhoneCallDiscardReasonBusy
+    "The call was ended before the conversation started. It was declined by the other party"
+
+    DISCONNECTED = raw.types.PhoneCallDiscardReasonDisconnect
+    "The call was ended before the conversation started. It was declined by the other party"
+
+    HUNG_UP = raw.types.PhoneCallDiscardReasonHangup
+    "The call was ended because one of the parties hung up"
+
+    UPGRADE_TO_CONFERENCE_CALL = raw.types.PhoneCallDiscardReasonMigrateConferenceCall
+    "The call was ended because it has been upgraded to a conference call"
