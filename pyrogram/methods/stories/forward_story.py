98c81e148ed6b5251a387e3fa3f05922257aa75e
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2025-05-09 20:16:38 +0530

    Add missing parameters in forward_story and Slightly Improve documentation (#170)

--- pyrogram/methods/stories/forward_story.py
+++ pyrogram/methods/stories/forward_story.py
@@ -33,10 +33,20 @@ class ForwardStory:
         message_thread_id: int = None,
         schedule_date: datetime = None,
         paid_message_star_count: int = None,
+        protect_content: bool = None,
+        allow_paid_broadcast: bool = None,
+        reply_parameters: "types.ReplyParameters" = None,
+        reply_markup: Union[
+            "types.InlineKeyboardMarkup",
+            "types.ReplyKeyboardMarkup",
+            "types.ReplyKeyboardRemove",
+            "types.ForceReply"
+        ] = None,
+        message_effect_id: int = None
     ) -> Optional["types.Message"]:
         """Forward story.
 
-        .. include:: /_includes/usable-by/users.rst
+        .. include:: /_includes/usable-by/users-bots.rst
 
         Parameters:
             chat_id (``int`` | ``str``):
@@ -65,6 +75,22 @@ class ForwardStory:
 
             paid_message_star_count (``int``, *optional*):
                 The number of Telegram Stars the user agreed to pay to send the messages.
+            
+            protect_content (``bool``, *optional*):
+                Pass True if the content of the message must be protected from forwarding and saving; for bots only.
+
+            allow_paid_broadcast (``bool``, *optional*):
+                Pass True to allow the message to ignore regular broadcast limits for a small fee; for bots only
+            
+            reply_parameters (:obj:`~pyrogram.types.ReplyParameters`, *optional*):
+                Description of the message to reply to.
+            
+            reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardRemove` | :obj:`~pyrogram.types.ForceReply`, *optional*):
+                Additional interface options. An object for an inline keyboard, custom reply keyboard,
+                instructions to remove reply keyboard or to force a reply from the user.
+            
+            message_effect_id (``int`` ``64-bit``, *optional*):
+                Unique identifier of the message effect to be added to the message; for private chats only.
 
         Returns:
             :obj:`~pyrogram.types.Message`: On success, the sent story message is returned.
@@ -88,10 +114,14 @@ class ForwardStory:
                 message="",
                 reply_to=await utils.get_reply_to(
                     self,
+                    reply_parameters,
                     message_thread_id
                 ),
                 allow_paid_stars=paid_message_star_count,
-
+                allow_paid_floodskip=allow_paid_broadcast,
+                reply_markup=await reply_markup.write(self) if reply_markup else None,
+                noforwards=protect_content,
+                effect=message_effect_id,
             )
         )
 
